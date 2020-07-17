e4019e8df026e01da354c875d7ad4bc5c3bf2bec
rjavier3 <54916537+rjavier3@users.noreply.github.com>
2020-07-17 02:42:45 -0600

    follow me links now works

--- russell-portfolio/js/main.js
+++ russell-portfolio/js/main.js
@@ -3,3 +3,8 @@ const clickButton = document.querySelector('.toggle-icon');
 clickButton.addEventListener('click', (event) => {
     document.querySelector('nav').classList.toggle('show-nav');
 });
+
+
+import Darkmode from 'darkmode-js';
+
+new Darkmode().showWidget();
