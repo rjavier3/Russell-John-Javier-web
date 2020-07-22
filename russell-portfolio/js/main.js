9f8f4dc00760aa568cd03322a71b2b5c53972024
rjavier3 <54916537+rjavier3@users.noreply.github.com>
2020-07-22 02:34:04 -0600

    dark mode attempt

--- russell-portfolio/js/main.js
+++ russell-portfolio/js/main.js
@@ -1,5 +1,17 @@
-//for hamburger menu
-const clickButton = document.querySelector('.toggle-icon');
-clickButton.addEventListener('click', (event) => {
+let hamburger = document.querySelector('.toggle-icon');
+let logo = document.querySelector('.logo');
+
+//hamburger menu
+hamburger.addEventListener('click', function(event) {
     document.querySelector('nav').classList.toggle('show-nav');
+});
+
+//dark / light mode
+logo.addEventListener('click', function(event){
+    if(document.documentElement.attributes('data-theme') === 'dark'){
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+    else{
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
 });
