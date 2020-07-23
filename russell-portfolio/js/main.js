667665fcc69ef25add6667a4b225b4189f983f50
rjavier3 <54916537+rjavier3@users.noreply.github.com>
2020-07-23 02:37:29 -0600

    update contact page

--- russell-portfolio/js/main.js
+++ russell-portfolio/js/main.js
@@ -5,13 +5,3 @@ let logo = document.querySelector('.logo');
 hamburger.addEventListener('click', function(event) {
     document.querySelector('nav').classList.toggle('show-nav');
 });
-
-//dark / light mode
-logo.addEventListener('click', function(event){
-    if(document.documentElement.attributes('data-theme') === 'dark'){
-        document.documentElement.setAttribute('data-theme', 'light');
-    }
-    else{
-        document.documentElement.setAttribute('data-theme', 'dark');
-    }
-});
