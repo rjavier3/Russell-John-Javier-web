ef466ae028f0315a4267a2141d6d69eea58d2c07
rjavier3 <54916537+rjavier3@users.noreply.github.com>
2020-07-23 03:55:51 -0600

    added dark mode

--- russell-portfolio/js/main.js
+++ russell-portfolio/js/main.js
@@ -1,7 +1,16 @@
 let hamburger = document.querySelector('.toggle-icon');
-let logo = document.querySelector('.logo');
+let themeCheckbox = document.querySelector('.theme-checkbox');
 
 //hamburger menu
 hamburger.addEventListener('click', function(event) {
     document.querySelector('nav').classList.toggle('show-nav');
+});
+
+//theme switch
+themeCheckbox.addEventListener('change', function(event){
+    if(this.checked){
+        document.documentElement.setAttribute('data-theme','dark');
+    } else{
+        document.documentElement.setAttribute('data-theme','light');
+    }
 });
