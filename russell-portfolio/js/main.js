92b836939e17b38e59677acd70586313e562d0b2
rjavier3 <54916537+rjavier3@users.noreply.github.com>
2020-10-07 22:49:51 -0600

    fixed html

--- russell-portfolio/js/main.js
+++ russell-portfolio/js/main.js
@@ -1,16 +1,5 @@
 let hamburger = document.querySelector('.toggle-icon');
-let themeCheckbox = document.querySelector('.theme-checkbox');
-
 //hamburger menu
 hamburger.addEventListener('click', function(event) {
     document.querySelector('nav').classList.toggle('show-nav');
-});
-
-//theme switch
-themeCheckbox.addEventListener('change', function(event){
-    if(this.checked){
-        document.documentElement.setAttribute('data-theme','dark');
-    } else{
-        document.documentElement.setAttribute('data-theme','light');
-    }
 });
