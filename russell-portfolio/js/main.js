2c847cb70229b96e06abbcc989cb26d6a10fb17a
Russell John Javier <teyjavier03@gmail.com>
2020-12-15 05:26:33 -0700

    Update main.js

--- russell-portfolio/js/main.js
+++ russell-portfolio/js/main.js
@@ -1,4 +1,5 @@
 let hamburger = document.querySelector('.toggle-icon');
+
 //hamburger menu
 hamburger.addEventListener('click', function(event) {
     document.querySelector('nav').classList.toggle('show-nav');
